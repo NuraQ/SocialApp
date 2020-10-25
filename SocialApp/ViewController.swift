6a988dc76d592eddeea0cdd640639e37d9e429d0
Nura <nura.qasrawi@gmail.com>
2020-10-25 15:26:00 +0300

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Asal 3 on 25/10/2020.
+//  Copyright © 2020 Asal 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
